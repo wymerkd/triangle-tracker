062af3e28a53289de646faa892435be30e6ccc6b
Student <example@example.com>
2019-11-13 10:17:16 -0800

    install webpack and tests

---
+++ src/main.js
@@ -0,0 +1,7 @@
+import $ from 'jquery';
+import 'bootstrap';
+import './styles.css';
+
+$(document).ready(function()) {
+
+})
---
+++ src/triangle.js
@@ -0,0 +1,20 @@
+export function Triangle(side1, side2, side3) {
+  this.side1 = side1;
+  this.side2 = side2;
+  this.side3 = side3;
+}
+
+Triangle.prototype.checkType = function() {
+
+   if ((typeof this.side1 === 'string') || (typeof this.side2 === 'string') || (typeof this.side3 === 'string')) {
+    return "enter a number";
+  } else if ((this.side1 > (this.side2 + this.side3)) || (this.side2 > (this.side1 + this.side3)) || (this.side3 > (this.side1 + this.side2))) {
+    return "not a triangle";
+  } else if ((this.side1 !== this.side2) && ((this.side1 !== this.side3)) && ((this.side2 !== this.side3))) {
+    return "scalene triangle";
+  } else if ((this.side1 === this.side2) && ((this.side1 === this.side3)) && ((this.side2 === this.side3))) {
+    return "equilateral triangle";
+  }  else if ((this.side1 === this.side2) || ((this.side1 === this.side3)) || ((this.side2 === this.side3))) {
+    return "isosceles triangle";
+  }
+};
